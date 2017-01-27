f489cc2cad22f05ff23fc62f49a4d3e88d886725
viktorm8 <cannarsa.daniele@gmail.com>
2017-01-27 18:12:15 +0100

    Let's get started!

--- codice/SentimentTweetAnalysis.py
+++ codice/SentimentTweetAnalysis.py
@@ -1,37 +1,8 @@
-#Questo è un commento di prova
-def LoadTweets():
-    #Questo è una funzione di prova
-    pass
+#Sentiment Analysis with Python
 
-# inserimento = input("Inserisci del testo\n")
-#
-# print(inserimento)
-#
-#
-# def Confronto(Primo,Secondo):
-#     if (Primo > Secondo):
-#         return 1
-#     elif (Primo == Secondo):
-#         return 0
-#     elif (Primo < Secondo):
-#         return -1
-#
-#
-# a = Confronto(10,21)
-# b = Confronto(1,1)
-# c = Confronto(90,1)
-#
-# print("Primo valore",a,"Secondo Valore",b,"Terzo Valore",c)
-#
-#
-# def NRigheVuote(n):
-#     while (n != 0):
-#         print("",".")
-#         n = n - 1
-#
-# NRigheVuote(10)
+#First Step: Access to tweet
 
-a = ["a","b","c"]
-b = a
-b[0] = "D"
-print(a)
+#Second Step: get the polarity of sentiment analysis
+
+#Coded By Daniele Cannarsa
+#Copyright 2017 - Tutti i diritti riservati
