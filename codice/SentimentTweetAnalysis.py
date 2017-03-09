4675d4cec3b24563378481468d251afe7c5798b4
viktorm8 <cannarsa.daniele@gmail.com>
2017-03-09 10:51:29 +0100

    add some lines of codes

--- codice/SentimentTweetAnalysis.py
+++ codice/SentimentTweetAnalysis.py
@@ -10,3 +10,24 @@
 
 #Coded By Daniele Cannarsa
 #Copyright 2017 - Tutti i diritti riservati
+
+import tweepy
+from textblob import textblob
+
+consumer_key = 'uAzSRIhykkh38dbD9f3rftHvJ'
+consumer_secret = 'xQbzXVy5cjz5P6feg6b3sIhMtYcbcnhSXzFsI8I4v2rTERpjlk'
+
+access_token = '3162981567-EKolFYFDlTpBgjSDfog8EEot91fXnb2UgdLNtAQ'
+access_token_secret = 'JWJTT5IvKOxae8X2zXglGBZcIQcKbzpqYaVfhmpBq9QFB'
+
+auth = tweepy.0AuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
