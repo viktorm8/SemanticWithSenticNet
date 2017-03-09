201aae46607de6b1f999ebf0d0fe51d88417f22d
viktorm8 <cannarsa.daniele@gmail.com>
2017-03-09 10:53:23 +0100

    some mods

--- codice/SentimentTweetAnalysis.py
+++ codice/SentimentTweetAnalysis.py
@@ -20,7 +20,7 @@ consumer_secret = 'xQbzXVy5cjz5P6feg6b3sIhMtYcbcnhSXzFsI8I4v2rTERpjlk'
 access_token = '3162981567-EKolFYFDlTpBgjSDfog8EEot91fXnb2UgdLNtAQ'
 access_token_secret = 'JWJTT5IvKOxae8X2zXglGBZcIQcKbzpqYaVfhmpBq9QFB'
 
-auth = tweepy.0AuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
