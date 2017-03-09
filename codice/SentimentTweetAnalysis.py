46277bfcb82cf2bff5cc516c4c4d307277cbc210
viktorm8 <cannarsa.daniele@gmail.com>
2017-03-09 10:33:32 +0100

    Add initial step on research work.

--- codice/SentimentTweetAnalysis.py
+++ codice/SentimentTweetAnalysis.py
@@ -4,5 +4,9 @@
 
 #Second Step: get the polarity of sentiment analysis
 
+#Third Step: Analysis of a case of a public interest.
+            #Example: OSINT on Trump Victory. Then verify the Trump Victory
+            #through OSINT of tweets.
+
 #Coded By Daniele Cannarsa
 #Copyright 2017 - Tutti i diritti riservati
